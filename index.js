033374f6588f6321ccc2d4db10149ad18036891f
Benjamin Whitehurst <61037364+benmw123@users.noreply.github.com>
2020-09-12 17:17:54 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function getDogImage() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(responseJson => 
+        $(createDogImage(responseJson)).appendTo('.results')) 
+      .catch(error => alert('Something went wrong. Try again later.'));
+  }
+  
+function createDogImage(responseJson) {
+  console.log(responseJson);
+  return `<img src="${responseJson.message}" class="results-img">`
+}
+
+function formListener() {
+    $('form').submit(event => {
+        event.preventDefault();
+        input = $('#dogAmount').val(); 
+        console.log(input); 
+        for (i=1; i <= input; i++) {
+          getDogImage();
+        }
+        
+    });
+
+}
+
+$(function() {
+    formListener();
+});
