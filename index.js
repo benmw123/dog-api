223badf75ab9ecaa7a8731c6cf191cdd49e47e89
Benjamin Whitehurst <61037364+benmw123@users.noreply.github.com>
2020-09-16 20:58:38 -0500

    js and html completed

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 function getDogImage() {
-    fetch('https://dog.ceo/api/breeds/image/random')
-      .then(response => response.json())
-      .then(responseJson => 
-        $(createDogImage(responseJson)).appendTo('.results')) 
-      .catch(error => alert('Something went wrong. Try again later.'));
-  }
-  
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(responseJson => 
+      $(createDogImage(responseJson)).appendTo('.results')) 
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
 function createDogImage(responseJson) {
-  console.log(responseJson);
-  return `<img src="${responseJson.message}" class="results-img">`
+console.log(responseJson);
+return `<img src="${responseJson.message}" class="results-img">`
 }
 
 
 
 function formListener() {
-    $('form').submit(event => {
-        event.preventDefault();
-        $('.results').empty(); 
-        input = $('#dogAmount').val(); 
-        console.log(input); 
-        for (i=1; i <= input; i++) {
-          getDogImage();
-        }
-    });
+  $('form').submit(event => {
+      event.preventDefault();
+      $('.results').empty(); 
+      input = $('#dogAmount').val(); 
+      console.log(input); 
+      for (i=1; i <= input; i++) {
+        getDogImage();
+      }
+  });
 }
 
 $(function() {
-    formListener();
+  formListener();
 });
