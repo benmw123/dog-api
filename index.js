fd92957c6a847c89e72818cf9d83dfc0340bbf9e
Benjamin Whitehurst <61037364+benmw123@users.noreply.github.com>
2020-09-27 13:37:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
-
-//function interacting with DogApi endpoint.
 function getDogImage() {
-  fetch(`https://dog.ceo/api/breed/${dogBreed}/images/random`)
+  fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
-    .then(responseJson =>
-      $(createDogImage(responseJson)))
-    .catch(error => alert('Try again'));
+    .then(responseJson => 
+      $(createDogImage(responseJson)).appendTo('.results')) 
+    .catch(error => alert('Something went wrong. Try again later.'));
 }
 
 function createDogImage(responseJson) {
-  //using the 404 message to create a fallback just in case a breed is not found. 
-  let message404 = "Breed not found (master breed does not exist)";
-  let dogImage = responseJson.message;
-  if (message404 === dogImage) {
-    alert("Breed not found. Try again");
-    $('.results').hide();
-  } else {
-    $('.results').append(`<img src="${dogImage}" class="results-img">`);
-  }
+console.log(responseJson);
+return `<img src="${responseJson.message}" class="results-img">`
 }
 
-//listening event for the form. 
+
+
 function formListener() {
   $('form').submit(event => {
-    event.preventDefault();
-    //clear results and add .show attribute.
-    $('.results').empty().show(); 
-    dogBreed = $('#dog-selector').val().replaceAll("-", "/");
-    getDogImage();
+      event.preventDefault();
+      $('.results').empty(); 
+      input = $('#dogAmount').val(); 
+      console.log(input); 
+      for (i=1; i <= input; i++) {
+        getDogImage();
+      }
   });
 }
 
-//calling the form listener.
-$(function () {
+$(function() {
   formListener();
 });
